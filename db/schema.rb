1e095bee10578e3521c0519da478929caab9a36d
takeruyamabi <t.yamabi@gmail.com>
2017-01-19 06:51:03 +0000

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161231082641) do
+ActiveRecord::Schema.define(version: 20170119064456) do
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
@@ -22,5 +22,15 @@ ActiveRecord::Schema.define(version: 20161231082641) do
 
   add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
