28cdf012fc0bb0e03b7ac37c934e554b39a9cf0c
takeruyamabi <t.yamabi@gmail.com>
2017-01-24 05:02:28 +0000

    user profile update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119064456) do
+ActiveRecord::Schema.define(version: 20170124020024) do
 
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20170119064456) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "location"
+    t.string   "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
